7a340bed21cb45e9f3e347e03cfdc4b4a0b5c504
petecarapetyan <pete@couldbe.net>
2016-06-08 18:21:01 -0500

    moved jammazwan.shared files to appropriate folders

--- src/main/java/jammazwan/xae/XaeRoutes.java
+++ src/main/java/jammazwan/xae/XaeRoutes.java
@@ -6,7 +6,7 @@ public class XaeRoutes extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("file:../jammazwan.shared/src/main/resources/data/?noop=true&fileName=name.csv")
+		from("file:../jammazwan.shared/src/main/resources/data/csv/?noop=true&fileName=name.csv")
 				.unmarshal("nameDataFormat").to("mock:names");
 		;
 	}
